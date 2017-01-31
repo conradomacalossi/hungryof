92dfba04aa7b56c7e225115b599ce6df193921b1
Conrado Macalossi <conrado.macalossi@gmail.com>
2017-01-31 01:50:40 -0200

    Catalog V 0.0.2

--- catalog/models.py
+++ catalog/models.py
@@ -13,6 +13,9 @@ class Category(models.Model):
         verbose_name_plural = 'Categorias'
         ordering = ['name']
 
+    def __str__(self):
+        return self.name
+
 class Ingredient(models.Model):
 
     name = models.CharField('Nome', max_length=100)
@@ -28,6 +31,9 @@ class Ingredient(models.Model):
         verbose_name = 'Ingrediente'
         verbose_name_plural = 'Ingredientes'
         ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
 
@@ -47,3 +53,6 @@ class Product(models.Model):
         verbose_name = 'Produto'
         verbose_name_plural = 'Produtos'
         ordering = ['name']
+
+    def __str__(self):
+        return self.name
